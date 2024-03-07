5425c4de089dc3bd29027487fa8908dbc69331f7
G-Mustafa1 <157616503+G-Mustafa1@users.noreply.github.com>
2024-03-08 03:37:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var name = "Name: Ghulam Mustafa"
+alert(name)
+
+var age = "Age: 17"
+alert(age)
+
+var course = "Course: Web and Mobile app development"
+alert(course)
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+alert("My email address: gmustufa1255@gmal.com")
+
+alert("I am reading\n'A smarter way to learn JavaScript'")
+
+// document.write(  "Yah! I can Write <b>HTML</b> content though JavaScript<br>“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+var desing = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(desing);
+
+document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
